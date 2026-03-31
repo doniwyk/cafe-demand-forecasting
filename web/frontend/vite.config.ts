7aefdb85e0829929586e18f9a203754cdf287e66
doniwyk <doniwyk@gmail.com>
2026-03-31 10:20:51 +0700

    feat: update dependencies and refactor app structure
    - install tanstack router
    - install effect
    - install shad

--- web/frontend/vite.config.ts
+++ web/frontend/vite.config.ts
@@ -1,10 +1,17 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [TanStackRouterVite({ quoteStyle: 'single' }), react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     proxy: {
       '/api': {
